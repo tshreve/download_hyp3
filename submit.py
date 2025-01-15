351906025e764e017b2c8d204cbd2a41d9c08629
tshreve <tshreve@utah.gov>
2025-01-15 12:17:19 -0700

    Add seasonal search

--- submit.py
+++ submit.py
@@ -10,14 +10,14 @@ from hyp3_sdk import HyP3
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print("Usage: submit.py job_name max_temp_base_days submit_true [filter_strength]")
+    if len(sys.argv) < 5:
+        print("Usage: submit.py job_name max_temp_base_days submit_true seasonal_true [filter_strength]")
         sys.exit(1)
-    elif len(sys.argv) == 4:
+    elif len(sys.argv) == 5:
         filt = 0.4
-    elif len(sys.argv) == 5: 
-        filt = float(sys.argv[4])
-  
+    elif len(sys.argv) == 6: 
+        filt = float(sys.argv[5])
+
 
 months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
@@ -30,21 +30,34 @@ job_specification = {
     "name": jname,
 }
 
-# search images available (seasonal search)
-options = {
-	'intersectsWith': 'POLYGON((-113.8789 37.5667,-113.4477 37.5667,-113.4477 37.9314,-113.8789 37.9314,-113.8789 37.5667))',
-	'dataset': 'SENTINEL-1',
-	'start': '2017-03-11T06:00:00Z',
-	'end': '2024-12-11T06:59:59Z',
-	'relativeOrbit': 93,
-	'processingLevel': 'SLC',
-	'flightDirection': 'Ascending',
-	'season': [
-		'121',
-		'335'
-	],
-	'maxResults': 250
-}
+#Update with Python output exported from ASF Vertex
+if sys.argv[4] == 'True':
+  # search images available (seasonal search)
+  options = {
+    'intersectsWith': 'POLYGON((-113.9971 37.5127,-113.2116 37.5127,-113.2116 38.0441,-113.9971 38.0441,-113.9971 37.5127))',
+    'dataset': 'SENTINEL-1',
+    'start': '2018-09-01T06:00:00Z',
+    'end': '2019-06-01T06:59:59Z',
+    'relativeOrbit': 93,
+    'processingLevel': 'SLC',
+    'flightDirection': 'Ascending',
+    'season': [
+      '121',
+      '335'
+    ],
+    'maxResults': 250
+  }
+elif sys.argv[4] == 'False':
+  # search images available (full search)
+  options = {
+	'intersectsWith': 'POLYGON((-112.4649 40.386,-111.5466 40.386,-111.5466 41.737,-112.4649 41.737,-112.4649 40.386))',
+    'dataset': 'SENTINEL-1',
+    'start': '2015-05-01T06:00:00Z',
+    'end': '2021-09-01T06:59:59Z',
+    'relativeOrbit': 100,
+    'processingLevel': 'SLC',
+    'maxResults': 1000
+  }
 
 # search for scenes
 results = asf.search(**options)
@@ -56,7 +69,7 @@ job = deepcopy(job_specification)
 
 # set temporal baseline constraints for nearest neighbor and year-long interferograms
 perpt_max = int(sys.argv[2])
-perpt_max_yrlong = 43
+perpt_max_yrlong = 30 #40
 results_r = results[::-1]
 
 # find pairs for each scene (nearest neighbor and year-long pairs) and write pairs and temporal baselines to file for double-checking
@@ -87,6 +100,7 @@ with open(f'{jname}_pairs.txt', 'w') as file:
           job['job_parameters']['granules'] = [ref.properties['sceneName'], sec.properties['sceneName']]
           jobs.append(job)
 
+#print(jobs[])
 # submit all jobs
 print(f'{len(jobs)} jobs')
 if sys.argv[3] == 'True':
@@ -94,7 +108,6 @@ if sys.argv[3] == 'True':
 
   # May need to split up job submission if too many jobs
   # Could add a try/except statement to catch errors
+
   hyp3.submit_prepared_jobs(jobs[0:100])
-  #hyp3.submit_prepared_jobs(jobs[101:200])
-  #hyp3.submit_prepared_jobs(jobs[201:300])
-  #hyp3.submit_prepared_jobs(jobs[301:400])
+  #hyp3.submit_prepared_jobs(jobs[100:200])
